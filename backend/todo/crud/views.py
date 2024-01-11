2265f38bb261b03c343930903681398a095659b0
rmnsrsta <ramanshrestha@varicon.com.au>
2024-01-11 21:19:48 +0545

    CRUD completed in function based views

--- backend/todo/crud/views.py
+++ backend/todo/crud/views.py
@@ -9,6 +9,7 @@ import io
 from .models import Student
 
 # Create your views here.
+@csrf_exempt
 def student_api(request):
     if request.method == 'GET':
         json_data = request.body
@@ -22,3 +23,37 @@ def student_api(request):
         stu = Student.objects.all()
         serializer = StudentSerializer(stu, many=True)
         return JsonResponse(serializer.data, safe=False)
+    
+    if request.method == 'POST':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        serializer = StudentSerializer(data=pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Student Added Successfull'}
+            return JsonResponse(res)
+        return JsonResponse(serializer.errors)
+
+    if request.method == 'PUT':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id')
+        stu = Student.objects.get(id=id)
+        serializer = StudentSerializer(stu, pythondata, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Student Data Update Successfull'}
+            return JsonResponse(res)
+        return JsonResponse(serializer.errors)
+
+    if request.method == 'DELETE':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id')
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        res = {'msg' : 'Student Deleted Successfully'}
+        return JsonResponse(res)

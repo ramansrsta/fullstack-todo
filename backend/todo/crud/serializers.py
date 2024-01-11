6c75f426c259a2b5331398ae1ad250745ab13b92
rmnsrsta <ramanshrestha@varicon.com.au>
2024-01-11 21:54:07 +0545

    Validation In Serializer Done

--- backend/todo/crud/serializers.py
+++ backend/todo/crud/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Student
 
+def name_starts_with_l(value):
+    if value[0].lower() != 'l':
+        raise serializers.ValidationError('Name Should Start With l')
+
 class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100)
+    name = serializers.CharField(max_length=100, validators=[name_starts_with_l])
     roll = serializers.IntegerField()
     city = serializers.CharField(max_length=100)
 
@@ -15,3 +19,14 @@ class StudentSerializer(serializers.Serializer):
         instance.city = validated_data.get('city', instance.city)
         instance.save()
         return instance
+
+    def validate_roll(self, value):
+        if value >= 200:
+            raise serializers.ValidationError('Seat Is Full')
+        return value
+
+    def validate(self,data):
+        nm = data.get('name')
+        if nm.lower() != 'luffy':
+            raise serializers.ValidationError('Name Must Be Luffy')
+        return data

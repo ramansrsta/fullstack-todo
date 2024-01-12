b9967ae091c56276249a7bef775f0570a423bd83
rmnsrsta <ramanshrestha@varicon.com.au>
2024-01-12 21:25:56 +0545

    Model Serializer With Validation Added

--- backend/todo/crud/serializers.py
+++ backend/todo/crud/serializers.py
@@ -1,32 +1,28 @@
 from rest_framework import serializers
 from .models import Student
 
-def name_starts_with_l(value):
-    if value[0].lower() != 'l':
-        raise serializers.ValidationError('Name Should Start With l')
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id','name' , 'roll', 'city']
+        # both are ways to make fields read only
+        # read_only_fields = ['name', 'roll']
+        # extra_kwargs = {
+        #     'name' : {"read_only" : True}
+        # }
 
-class StudentSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=100, validators=[name_starts_with_l])
-    roll = serializers.IntegerField()
-    city = serializers.CharField(max_length=100)
-
-    def create(self, validated_data):
-        return Student.objects.create(**validated_data)
-    
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.roll = validated_data.get('roll', instance.roll)
-        instance.city = validated_data.get('city', instance.city)
-        instance.save()
-        return instance
-
-    def validate_roll(self, value):
+    def validate_roll(self,value):
         if value >= 200:
             raise serializers.ValidationError('Seat Is Full')
         return value
 
-    def validate(self,data):
+    def validate(self, data):
         nm = data.get('name')
-        if nm.lower() != 'luffy':
-            raise serializers.ValidationError('Name Must Be Luffy')
+        print(nm)
+        if nm.lower() == 'luffy':
+            raise serializers.ValidationError('Name Cannot Be Luffy')
         return data
+            
+
+
+

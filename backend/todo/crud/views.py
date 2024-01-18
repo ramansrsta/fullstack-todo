97b01475ea3539517b33aa3613d8e8adad6127f6
rmnsrsta <ramanshrestha@varicon.com.au>
2024-01-18 19:51:28 +0545

    Added Generic View Sets

--- backend/todo/crud/views.py
+++ backend/todo/crud/views.py
@@ -1,26 +1,15 @@
 from .models import Student
 from .serializers import StudentSerializer
-from rest_framework.generics import GenericAPIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
-class LCStudentAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+class LCStudentAPI(ListCreateAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-   
 
-class RUDStudentAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+class RUDStudentAPI(RetrieveUpdateDestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+
 
 
  

8e212741b76789da0da2339c6237fc7bf96f30cd
rmnsrsta <ramanshrestha@varicon.com.au>
2024-01-29 19:02:55 +0545

    Added function based permission

--- backend/todo/crud/views.py
+++ backend/todo/crud/views.py
@@ -1,26 +1,54 @@
-from .models import Student
-from .serializers import StudentSerializer
-
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.views import View
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-from rest_framework import status
-from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
-from .permissions import MyPermission
-
-class StudentModelViewSet(viewsets.ModelViewSet):
-    queryset = Student.objects.all()
-    serializer_class = StudentSerializer
-    authentication_classes = [SessionAuthentication]
-    permission_classes = [MyPermission]
-
-
-    
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 
+import io
 
+from .serializers import StudentSerializer
+from .models import Student
 
- 
+@api_view(['GET','POST', 'PUT', 'DELETE'])
+@authentication_classes([BasicAuthentication])
+@permission_classes([IsAuthenticated])
+def student_api(request,pk=None):
+    if(request.method == 'GET'):
+        id = pk
+        if id is not None:
+            stu = Student.objects.get(id=id)
+            serializer = StudentSerializer(stu)
+            return Response(serializer.data)
+        stu = Student.objects.all()
+        serializer = StudentSerializer(stu, many=True)
+        return Response(serializer.data)
+    if(request.method == 'POST'):
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Created'})
+        return Response(serializer.errors)
 
+    if(request.method == 'DELETE'):
+        id = pk
+        stu = Student.objects.get(pk=id)
+        stu.delete()
+        return Response({'msg': 'Data Deleted'})
+
+    if(request.method == 'PUT'):
+        id = pk
+        stu = Student.objects.get(pk=id)
+        serializer = StudentSerializer(stu,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'msg': 'Data Updated'})
+        return Response(serializer.errors)
--- backend/todo/todo/urls.py
+++ backend/todo/todo/urls.py
@@ -14,17 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from crud import views
-
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-
-router.register('studentapi',views.StudentModelViewSet,basename="student")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('auth/', include('rest_framework.urls',namespace="rest_framework"))
+    path('studentapi/' , views.student_api),
+    path('studentapi/<int:pk>' , views.student_api)
 ]

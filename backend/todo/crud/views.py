e78409a7d78958630fa4af716adfadc2df42e780
rmnsrsta <ramanshrestha@varicon.com.au>
2024-01-29 18:53:15 +0545

    Custom Permission Added

--- backend/todo/crud/views.py
+++ backend/todo/crud/views.py
@@ -7,12 +7,13 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+from .permissions import MyPermission
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [SessionAuthentication]
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [MyPermission]
 
 
     

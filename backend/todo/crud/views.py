a5f8b06a52dcd948e00c5598a7054c45c2460ecc
rmnsrsta <ramanshrestha@varicon.com.au>
2024-01-31 21:04:27 +0545

    token implementation

--- backend/todo/crud/views.py
+++ backend/todo/crud/views.py
@@ -5,14 +5,14 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAdminUser]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
     

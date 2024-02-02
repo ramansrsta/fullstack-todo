aa3eab8e402ea076cbd64bfa52add28cb6095dff
rmnsrsta <ramanshrestha@varicon.com.au>
2024-02-02 21:41:52 +0545

    JWT implementation using simple jwt

--- backend/todo/crud/views.py
+++ backend/todo/crud/views.py
@@ -8,10 +8,12 @@ from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [TokenAuthentication]
+    authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
 
 
--- backend/todo/todo/urls.py
+++ backend/todo/todo/urls.py
@@ -20,6 +20,8 @@ from crud import views
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken.views import obtain_auth_token
 
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView, TokenVerifyView
+
 router = DefaultRouter()
 
 router.register('studentapi',views.StudentModelViewSet,basename="student")
@@ -27,5 +29,7 @@ router.register('studentapi',views.StudentModelViewSet,basename="student")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('gettoken/', obtain_auth_token )
+    path('gettoken/', TokenObtainPairView.as_view(),name="token_obtain_pair"),
+    path('refreshtoken/', TokenRefreshView.as_view(),name="token_refresh"),
+    path('verifytoken/', TokenVerifyView.as_view(),name="token_verify"),
 ]

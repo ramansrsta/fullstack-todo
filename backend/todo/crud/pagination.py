7019cb3f6858dd70e743f11f157218521e4fb60e
rmnsrsta <ramanshrestha@varicon.com.au>
2024-02-07 18:24:54 +0545

    Curson pagination added

--- backend/todo/crud/pagination.py
+++ backend/todo/crud/pagination.py
@@ -12,3 +12,11 @@ class PaginationLimitOffset(LimitOffsetPagination):
     limit_query_param = 'limit'
     offset_query_param = 'offset'
     last_page_strings = 'end'
+
+class PaginationCursor(CursorPagination):
+    ordering = 'id'
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 10
+    cursor_query_param = 'cursor'
+    last_page_strings = 'end'
